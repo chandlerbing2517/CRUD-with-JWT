104c81588ffca603b56a8e2e75156b4676edd633
sajal <Jainsajal519@gmail.com>
2023-12-12 01:29:35 +0530

    create header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,23 @@
-import { BrowserRouter,Route,Routes } from "react-router-dom" ;
-
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/profile";
+import Header from "./components/header";
 export default function App() {
-  return 
-  <BrowserRouter>
-  
-  </BrowserRouter>
-  
-  
+  return (
+    <BrowserRouter>
+        {/*header*/}
+<Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
