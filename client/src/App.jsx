657f9a9f6a608a18dafdaa466a6f70571a90c33f
sajal <Jainsajal519@gmail.com>
2023-12-11 19:27:29 +0530

    Second commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import { BrowserRouter,Route,Routes } from "react-router-dom" ;
 
 export default function App() {
-  return (
-    <div>
-      App
-    </div>
-  )
+  return 
+  <BrowserRouter>
+  
+  </BrowserRouter>
+  
+  
 }
