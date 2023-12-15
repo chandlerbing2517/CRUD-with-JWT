f92b2cd2d0233e29712c6908707048164de8d97a
sajal <Jainsajal519@gmail.com>
2023-12-15 23:06:06 +0530

    controller

--- Backend/controller/auth.controller.js
+++ Backend/controller/auth.controller.js
@@ -1,6 +1,6 @@
 import User from '../model/user.model.js'
 import brycptjs from 'bcryptjs';
-export const SignUp=async(req,res)=>{
+export const SignUp=async(req,res,next)=>{
     const {username,email,password}=req.body;
     const hashpass=brycptjs.hashSync(password,10);
     const newUser=new User({username,email,password:hashpass});
@@ -8,6 +8,6 @@ export const SignUp=async(req,res)=>{
         await newUser.save();
         res.status(201).json({message: "User created"});
     } catch (error) {
-        res.status(500).json(error.message);
+        next(error);
     }
 }
